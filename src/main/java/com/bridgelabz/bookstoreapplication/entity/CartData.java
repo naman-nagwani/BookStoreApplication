0d41e30d1d59bc0b2a72cc4c1f80df462d52154e
“naman-nagwani” <namannagwani@gmail.com>
2022-10-30 12:58:14 +0530

    Cart Model

---
+++ src/main/java/com/bridgelabz/bookstoreapplication/entity/CartData.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.bookstoreapplication.entity;
+
+import com.bridgelabz.bookstoreapplication.DTO.CartDTO;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "cart_data")
+public class CartData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "cart_id")
+    private int cartId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private UserLoginData userLoginData;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private BookData bookData;
+
+    private int quantity;
+    private int totalPrice;
+
+    public CartData(UserLoginData userLoginData,BookData bookData,int quantity,int totalPrice) {
+        this.userLoginData = userLoginData;
+        this.bookData = bookData;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+}
